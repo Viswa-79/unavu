0713b10ef3f69e0592fe0920d3f0416006f0e502
Vishveshwar <128994757+Viswa-79@users.noreply.github.com>
2025-01-07 13:44:15 +0530

    Update home.dart

--- lib/app/home.dart
+++ lib/app/home.dart
@@ -54,3 +54,61 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+
+<script>
+function get_process_details(id,value) {
+  var c=id.substr(11);
+  //alert(c);
+				
+  if (value != "") {
+    
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+
+r = xmlhttp.responseText;
+//alert(r);
+const data = JSON.parse(r);
+if(data.sts == 'ok'){
+  
+
+                  
+}
+
+      }
+    };
+    xmlhttp.open("GET","ajax/getprocess.php?id="+value,true);
+    xmlhttp.send();
+  }
+}
+</script>
+
+
+<?php
+include "../config.php";
+if(!empty($_GET['id']))
+{
+    $data = array();
+    
+    $sql = "SELECT * FROM dia WHERE id = '".$_GET['id']."'";
+    $result = mysqli_query($conn, $sql);
+    $count=mysqli_num_rows($result);
+
+    $row = mysqli_fetch_assoc($result);   
+    //get user data from the database
+    
+    if($count > 0){
+       
+        $data['sts'] = 'ok';
+        $data['id'] = $row['id'];
+        $data['dia'] = $row['dia'];
+        
+
+    
+    }
+    
+    //returns data as JSON format
+    echo json_encode($data);
+}
+?>
